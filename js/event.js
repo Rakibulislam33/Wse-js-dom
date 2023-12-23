c800637e25ca7715c51a9f3a367127ede1779b48
Rakibul islam <62441272+Rakibulislam33@users.noreply.github.com>
2023-12-23 09:02:06 +0600

    Add files via upload
    Javascript Dom Project

---
+++ js/event.js
@@ -0,0 +1,13 @@
+
+function makeblue(){
+    document.body.style.backgroundColor = 'blue';
+}
+function makegreen(){
+    document.body.style.backgroundColor = 'green';
+}
+
+
+
+const makeItGreen = document.getElementById('make-it-green');
+
+makeItGreen.onclick = makegreen;
